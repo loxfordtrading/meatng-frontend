646620e6c473e7522541a81a0699f93d0a9e293f
User <user@Users-MacBook-Air.local>
2026-04-16 04:30:27 +0100

    back Button added

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -10,7 +10,7 @@ export const BackButton = () => {
       className="h-9 w-12 rounded-full"
       onClick={() => navigate(-1)}
     >
-      <ArrowLeft className="h-5 w-5 text-muted-foreground hover:text-primary" />
+      <ArrowLeft className="h-6 w-6 text-primary hover:text-primary" />
     </button>
   );
 };
